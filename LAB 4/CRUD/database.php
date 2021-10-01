65e0999f505817f10a134090c7a9f704cc79e117
Maheshwor Poudel <58098559+sugam-hub@users.noreply.github.com>
2021-10-01 15:49:56 +0545

    Update database.php

--- LAB 4/CRUD/database.php
+++ LAB 4/CRUD/database.php
@@ -4,7 +4,7 @@
     $password= "";
     $database= "web";
 
-    $conn = new mysql($host, $username,$password,$databse);
+    $conn = new mysqli($host, $username,$password,$databse);
     if($conn->connect_errno):
         echo "Database Connection Failed:" . $conn->connect_error;
         exit();
